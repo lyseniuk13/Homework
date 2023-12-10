c7c31acf083e46519522866cde45c408eb6e5814
lyseniuk13 <117768470+lyseniuk13@users.noreply.github.com>
2023-12-10 17:55:15 +0200

    main.js

--- HW2/main.js
+++ HW2/main.js
@@ -1,1 +1,14 @@
+{	const hoursPerDay = 24;
+	const minutePerHour = 60;
+	const secondsPerMinute = 60;
+	const days = 7;
 
+	let hour = days * hoursPerDay;
+	let min = hour * minutePerHour;
+	let sec = min * secondsPerMinute;
+	console.log(hour, min, sec);
+}
+
+{
+
+}
