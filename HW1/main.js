f6dea908cfd234e3f9cd7cb5a658b02b0643ea95
lyseniuk13 <117768470+lyseniuk13@users.noreply.github.com>
2023-12-10 17:52:50 +0200

    main.js

--- HW1/main.js
+++ HW1/main.js
@@ -1,1 +1,43 @@
+ {
+	const userCount = 2;
+	const user1Name = 'Maria';
+	const user2Name = 'Lui';
+	const user3Name = 'Pier';
+	const location = 'Cafe';
+	const user2Amaze = true;
+	const coffeeCount = 1; 
+}
 
+{
+  	const city = 'New York'; 
+  	const time = '15:00';
+  	const user1Name = 'Michael';
+  	const user2Name = 'Amelia';
+  	const user2Painter = true;
+  	const place = 'exposition';
+  	const user3Name = 'Simon';
+  	const userCont = 3; 
+}
+
+{
+	const country = 'Goa';
+	const place = 'Beach'; 
+	const time = '11:00';
+	const user1Name = 'Kate';
+	const user2Name = 'James';
+	const user3Name = 'Radje';
+	const user3Proffesion = 'guide';
+	const user4Name = 'Leo';
+}
+
+{
+	const place = 'party';
+	const city = 'Tokio';
+	const time = '21:00';
+	const user1Name = 'Yuka';
+	const user2Name = 'Daigo'; 
+	const user3Name = 'Miuko';
+	const user1Proffesion = 'museum curator';
+	const user2Proffesion = 'architect';
+	const user3Proffesion = 'photographer';
+}
