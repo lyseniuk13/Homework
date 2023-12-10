b3da9d63273860ce3e6b2bfedf352fd7910f09ce
lyseniuk13 <117768470+lyseniuk13@users.noreply.github.com>
2023-12-10 17:48:09 +0200

    Delete 13 directory

--- 13/13.js
+++
@@ -1,6 +0,0 @@
-const questionNumberplace = 'Введіть номер свого місця';
-const place = prompt(questionNumberplace);
-const coupe = place <= 36 ? Math.trunc((place-1)/4+1) : Math.trunc((54-place)/2+1);
-const shelf = place % 2 == 0 ? 'Верхнє місце' : 'Нижне місце';
-const result = `Купе №${coupe}, ${shelf}`;
-alert (result)
