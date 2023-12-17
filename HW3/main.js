e3f020d1c8d4ffa19ab6d9283a812068ee7a60e6
lyseniuk13 <117768470+lyseniuk13@users.noreply.github.com>
2023-12-17 15:59:23 +0200

    main.js

--- HW3/main.js
+++ HW3/main.js
@@ -5,3 +5,18 @@ const pi = 3.14;
 const volume = pi * radius ** 2 * height;
 console.log(volume);
 }
+
+{
+const day = Math.floor(Math.random()*7+1);
+
+if(day === 1){
+	console.log('Sunday');
+}else{
+	if(day === 2) console.log('Monday');
+	if(day === 3) console.log('Tuesday');
+	if(day === 4) console.log('Wednesday');
+  if(day === 5) console.log('Thursday');
+  if(day === 6) console.log('Friday');
+  if(day === 7) console.log('Saturday');
+}
+}
