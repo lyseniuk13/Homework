5b028f7e899dd0ae4530c490c5f1cb061da7f989
lyseniuk13 <117768470+lyseniuk13@users.noreply.github.com>
2023-12-17 15:50:29 +0200

    main.js

---
+++ HW3/main.js
@@ -0,0 +1,7 @@
+{
+const radius = Math.floor(Math.random()*10+1);
+const height = Math.floor(Math.random()*10+1); 
+const pi = 3.14; 
+const volume = pi * radius ** 2 * height;
+console.log(volume);
+}
