430d07e6bd54e9c4ed7aa73d970f9089b027a9db
lyseniuk13 <117768470+lyseniuk13@users.noreply.github.com>
2023-12-17 16:54:57 +0200

    main.js

--- HW4/main.js
+++ HW4/main.js
@@ -1,1 +1,37 @@
+{
 
+}
+
+
+{
+
+}
+
+
+{
+const operator = Math.floor(Math.random()*90+10);
+const number = Math.floor(Math.random()*9000000+1000000)
+const phone = `+38(0${operator})${number}`;
+console.log(phone);
+}
+
+
+{
+const number = Math.floor(Math.random()*7+1);
+
+if(number === 1){
+	console.log('Red');
+}else{
+	if(number === 2) console.log('Orange');
+	if(number === 3) console.log('Yellow');
+	if(number === 4) console.log('Green');
+  if(number === 5) console.log('Blue');
+  if(number === 6) console.log('Indigo');
+  if(number === 7) console.log('Violet');
+}
+}
+
+
+{
+
+}
